c4c01c5152a987d93ab71c1e979e240ed6ed8105
Madhan <madhanmadhan8015@gmail.com>
2026-08-11 14:17:17 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 8/11/2026, 2:17:17 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0)    
+        return true;
+        else
+        return false;
+    }
+}
